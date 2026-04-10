ffcc0792fd6f5f768dcd55ff6a9cd8b272c0acd7
michaelasilalahi <michael.amanda.silalahi@gmail.com>
2026-04-10 21:35:28 +0700

    change database url

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # databasse address
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:michaelasilalahi@localhost:5432/xpensa_db"
+DEFAULT_DATABASE_URL = "postgresql://postgres:michaelasilalahi@localhost:5432/xpensa_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
 
 # engine untuk mengantar bolak-balik antara databse
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
