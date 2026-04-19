9876c4ad97110d2c4d9b5c47d922c0e2b9a75025
michaelasilalahi <michael.amanda.silalahi@gmail.com>
2026-04-20 04:52:10 +0700

    fe: fixing bug todays expenses

--- xpensa/src/features/todays-expenses/api/todayExpense.api.ts
+++ xpensa/src/features/todays-expenses/api/todayExpense.api.ts
@@ -1,12 +1,17 @@
 import { apiClient } from '@/config/apiClient';
 import { API_ENDPOINTS } from '@/constants/endpoints';
 import { Expense } from '@/features/todays-expenses/types/todayExpense.interface';
+import { getCurrentLocalDateString } from '@/features/todays-expenses/utils/todayExpense.helpers';
 
 export const todaysExpensesApi = {
   getTodaysExpenses: async (userId: string): Promise<Expense[]> => {
     try {
       const url = API_ENDPOINTS.TODAY_EXPENSE.GET(userId);
-      const response = await apiClient.get<Expense[]>(url);
+      const response = await apiClient.get<Expense[]>(url, {
+        params: {
+          client_date: getCurrentLocalDateString(),
+        },
+      });
       return response.data;
     } catch (error) {
       console.error('Error fetching todays expenses:', error);
